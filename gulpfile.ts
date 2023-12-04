1c5285141719f736d9d29f0f019295a4c4fc9e9b
Bill Garrett <garrett@astralfrontier.org>
2023-12-04 15:03:18 -0800

    Gulpfile: use const filenames

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,19 +6,25 @@ import pug from 'pug'
 import sass from 'sass'
 import postcss from 'gulp-postcss'
 import purgecss from '@fullhuman/postcss-purgecss'
+import Vinyl from 'vinyl'
 
 type GulpCallback = (err?: any | undefined) => void
 
 const INPUT_DIR = path.join(__dirname, 'src')
 const OUTPUT_DIR = path.join(__dirname, 'dist')
-const SHEET_HTML = `${packageJson.name}.html`
-const SHEET_CSS = `${packageJson.name}.css`
-const SHEET_PREVIEW = 'preview.png'
 
-function defaultTask(cb: GulpCallback) {
-  // place code for your default task here
-  cb()
-}
+// Source files must exist for the pipeline to function
+
+const VARIABLES_JSON = 'variables.json' // Local variables for SASS etc.
+const SHEET_MD = 'sheet.md' // Instructions for sheet use
+const SHEET_PUG = 'sheet.pug' // The main HTML for the sheet
+const SHEET_SASS = 'sheet.sass' // SASS/SCSS for the sheet layout
+
+// Files in the output directory
+const SHEET_JSON = 'sheet.json' // Metadata for the sheet, used by Roll20
+const SHEET_HTML = `${packageJson.name}.html` // The Sheet HTML
+const SHEET_CSS = `${packageJson.name}.css` // The Sheet CSS
+const SHEET_PREVIEW = 'preview.png' // A preview image
 
 function ensureOutputDir(cb: GulpCallback) {
   fs.ensureDir(OUTPUT_DIR, cb)
@@ -31,23 +37,23 @@ function createSheetJson(cb: GulpCallback) {
     authors: packageJson.author,
     roll20userid: packageJson.description,
     preview: SHEET_PREVIEW,
-    instructions: fs.readFileSync(path.join(INPUT_DIR, 'sheet.md')).toString(),
+    instructions: fs.readFileSync(path.join(INPUT_DIR, SHEET_MD)).toString(),
     legacy: false,
   }
 
-  fs.writeJSON(path.join(OUTPUT_DIR, 'sheet.json'), sheetJson, cb)
+  fs.writeJSON(path.join(OUTPUT_DIR, SHEET_JSON), sheetJson, cb)
 }
 
 function createSheetHtml(cb: GulpCallback) {
-  const locals: any = fs.readJSONSync(path.join(INPUT_DIR, 'variables.json'))
-  const html = pug.compileFile(path.join(INPUT_DIR, 'sheet.pug'), {
+  const locals: any = fs.readJSONSync(path.join(INPUT_DIR, VARIABLES_JSON))
+  const html = pug.compileFile(path.join(INPUT_DIR, SHEET_PUG), {
     basedir: INPUT_DIR,
   })({ ...locals, package: packageJson })
   fs.writeFile(path.join(OUTPUT_DIR, SHEET_HTML), html, cb)
 }
 
 function createSheetCss(cb: GulpCallback) {
-  const css = sass.compile(path.join(INPUT_DIR, 'sheet.sass'), {
+  const css = sass.compile(path.join(INPUT_DIR, SHEET_SASS), {
     style: 'compressed',
   }).css
   fs.writeFile(path.join(OUTPUT_DIR, SHEET_CSS), css, cb)
