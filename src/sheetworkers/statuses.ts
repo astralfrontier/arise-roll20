4a845edbad73802f86f781066aeb5cca9b9c4e1f
Bill Garrett <garrett@astralfrontier.org>
2023-12-31 01:59:20 -0800

    feat: Add a button to reduce status duration automatically

--- src/sheetworkers/statuses.ts
+++ src/sheetworkers/statuses.ts
@@ -16,3 +16,23 @@ on('change:repeating_statuses:status', (event) => {
     setAttrs(O)
   }
 })
+
+function reduceStatusDurations(amount: number) {
+  getSectionIDs('statuses', function (idarray) {
+    const attrs = idarray.map((name) => `repeating_statuses_${name}_duration`)
+    getAttrs(attrs, (values) => {
+      for (let value of Object.keys(values)) {
+        values[value] = `${Math.max(
+          0,
+          parseInt(values[value] || '0') - amount
+        )}`
+      }
+      setAttrs(values)
+      // TODO: remove statuses with duration 0
+    })
+  })
+}
+
+on('clicked:statusturn', (event) => {
+  reduceStatusDurations(1)
+})
